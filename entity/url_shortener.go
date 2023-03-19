84e960c2fede1cd709dd3bbe38ae23f24ddc6ff4
InoHex <mtgpmn2013@gmail.com>
2023-03-19 12:13:04 +0700

    feat : add entity url shortener

--- entity/url_shortener.go
+++ entity/url_shortener.go
@@ -1,1 +1,15 @@
 package entity
+
+import "github.com/gofrs/uuid"
+
+type UrlShortener struct {
+	ID       uuid.UUID `gorm:"primary_key;not_null" json:"id"`
+	ShortUrl string    `json:"short_url"`
+	LongUrl  string    `json:"long_url"`
+	Views    uint64    `json:"views"`
+	Private  bool      `json:"private"`
+	UserID   uuid.UUID `gorm:"foreignKey" json:"user_id"`
+	User     *User     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"user,omitempty"`
+
+	// Timestamp
+}
