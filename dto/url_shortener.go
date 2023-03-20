dbf8853eca5383e677b2859995f3f1da257e941b
Moch. Taslam Gustino P <93267604+gustino7@users.noreply.github.com>
2023-03-20 10:57:36 +0700

    feat : add dto url shortener
    Co-authored-by: Rencist <dafianakhdan@gmail.com>

--- dto/url_shortener.go
+++ dto/url_shortener.go
@@ -1,1 +1,31 @@
 package dto
+
+import (
+	"url-shortener-backend/entity"
+
+	"github.com/google/uuid"
+)
+
+type UrlShortenerCreateDTO struct {
+	ID         	uuid.UUID `gorm:"primary_key;not_null" json:"id"`
+	ShortUrl   	string    `json:"short_url" binding:"required"`
+	LongUrl    	string    `json:"long_url" binding:"required"`
+	Views 		uint64    `json:"views" form:"views"`
+	Is_Private 	*bool     `json:"is_private" binding:"required"`
+	Is_Feeds   	*bool     `json:"is_feeds" binding:"required"`
+	
+	UserID   	uuid.UUID 		`gorm:"foreignKey" json:"user_id"`
+	User     	*entity.User  	`gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"user,omitempty"`
+
+	Password   	string    `json:"password"`
+}
+
+type UrlShortenerUpdateDTO struct {
+	ID         uuid.UUID    `gorm:"primary_key;not_null" json:"id"`
+	ShortUrl   string 		`json:"short_url"`
+	LongUrl    string 		`json:"long_url"`
+	Is_Private bool   		`json:"is_private"`
+	Is_Feeds   bool   		`json:"is_feeds"`
+	
+	Password   string 		`json:"password"`
+}
