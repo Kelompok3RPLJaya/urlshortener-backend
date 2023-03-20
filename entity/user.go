7d90f9a67f88a6187466d6642e372d4257a47b4c
Muhamad Dafian Zaki Akhdan <91055469+Rencist@users.noreply.github.com>
2023-03-20 10:42:41 +0700

    feat: entity user

--- entity/user.go
+++ entity/user.go
@@ -1,1 +1,15 @@
 package entity
+
+import (
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        	uuid.UUID   `gorm:"primary_key;not_null" json:"id"`
+	Name 		string 		`json:"name"`
+	Email 		string 		`json:"email" binding:"email"`
+	Password 	string  	`json:"password"`
+	Role		string		`json:"role"`
+	
+	// Timestamp
+}
