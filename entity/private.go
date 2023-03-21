5041322f42950e5dfb047f342e9eca771542e0e4
Ahmad Fauzan Alghifari <fauzanalghifari963@gmail.com>
2023-03-21 19:20:35 +0700

    feat : add private entity

--- entity/private.go
+++ entity/private.go
@@ -1,1 +1,9 @@
 package entity
+
+import "github.com/google/uuid"
+
+type userPrivate struct {
+	ID             uuid.UUID `gorm:"primary_key;not_null"`
+	Password       string    `json:"password"`
+	UrlShortenerID uuid.UUID `gorm:"foreignkey:ID"`
+}
