758033f71b4359e821bfe42e804367fe9e745536
Rencist <dafianakhdan@gmail.com>
2023-03-22 18:33:01 +0700

    feat: config template

--- config/database.go
+++ config/database.go
@@ -1,1 +1,45 @@
 package config
+
+import (
+	"fmt"
+	"os"
+	"url-shortener-backend/entity"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupDatabaseConnection() *gorm.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v TimeZone=Asia/Jakarta", dbHost, dbUser, dbPass, dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	if err := db.AutoMigrate(
+		entity.User{},
+		entity.UrlShortener{},
+		entity.Private{},
+	); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	return db
+}
+
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	dbSQL.Close()
+}
--- middleware/authenticate.go
+++ middleware/authenticate.go
@@ -1,1 +1,115 @@
 package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"url-shortener-backend/common"
+	"url-shortener-backend/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(jwtService service.JWTService, isAdmin bool) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			response := common.BuildErrorResponse("Gagal Memproses Request", "Token Tidak Ditemukan", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		if !strings.Contains(authHeader, "Bearer ") {
+			response := common.BuildErrorResponse("Gagal Memproses Request", "Token Tidak Valid", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		authHeader = strings.Replace(authHeader, "Bearer ", "", -1)
+		token, err := jwtService.ValidateToken(authHeader)
+		if err != nil {
+			response := common.BuildErrorResponse("Gagal Memproses Request", "Token Tidak Valid", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		if !token.Valid {
+			response := common.BuildErrorResponse("Gagal Memproses Request", "Akses Ditolak", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		if isAdmin {
+			userRole, err := jwtService.GetUserRoleByToken(authHeader)
+			if err != nil {
+				response := common.BuildErrorResponse("Gagal Memproses Request", "Token Tidak Valid", nil)
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+				return
+			}
+			fmt.Println(userRole)
+			if userRole != "admin" {
+				response := common.BuildErrorResponse("Gagal Memproses Request", "Role User Tidak Memiliki Akses ke Endpoint Ini", nil)
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+				return
+			}
+		}
+
+		userID, err := jwtService.GetUserIDByToken(authHeader)
+		if err != nil {
+			response := common.BuildErrorResponse("Gagal Memproses Request", err.Error(), nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		ctx.Set("token", authHeader)
+		ctx.Set("userID", userID)
+		ctx.Next()
+	}
+}
+
+func CreateShortUrlAuthenticate(jwtService service.JWTService, isAdmin bool) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			return
+		}
+		if !strings.Contains(authHeader, "Bearer ") {
+			response := common.BuildErrorResponse("Gagal Memproses Request", "Token Tidak Valid", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		authHeader = strings.Replace(authHeader, "Bearer ", "", -1)
+		token, err := jwtService.ValidateToken(authHeader)
+		if err != nil {
+			response := common.BuildErrorResponse("Gagal Memproses Request", "Token Tidak Valid", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		if !token.Valid {
+			response := common.BuildErrorResponse("Gagal Memproses Request", "Akses Ditolak", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		if isAdmin {
+			userRole, err := jwtService.GetUserRoleByToken(authHeader)
+			if err != nil {
+				response := common.BuildErrorResponse("Gagal Memproses Request", "Token Tidak Valid", nil)
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+				return
+			}
+			fmt.Println(userRole)
+			if userRole != "admin" {
+				response := common.BuildErrorResponse("Gagal Memproses Request", "Role User Tidak Memiliki Akses ke Endpoint Ini", nil)
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+				return
+			}
+		}
+
+		userID, err := jwtService.GetUserIDByToken(authHeader)
+		if err != nil {
+			response := common.BuildErrorResponse("Gagal Memproses Request", err.Error(), nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		ctx.Set("token", authHeader)
+		ctx.Set("userID", userID)
+		ctx.Next()
+	}
+}
